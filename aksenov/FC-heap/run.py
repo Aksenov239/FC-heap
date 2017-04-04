34e948e08a83abe9bf69edfb4657aefa5f6049c5
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-04 16:31:25 +0000

    small fix

--- aksenov/FC-heap/run.py
+++ aksenov/FC-heap/run.py
@@ -15,7 +15,7 @@ iterations = 5
 procs = [1, 2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 63]
 sizes = [800000, 10000000, 20000000]
 
-max_proc = int(sys.argv[2])
+max_proc = int(sys.argv[1])
 
 benchmarks=["fc.parallel.FCParallelHeap",
            "fc.parallel.FCHalfParallelHeap",
