00568120242f06a73297e471b24fe0b8ef7ed7aa
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-04 16:16:07 +0000

    add run script

---
+++ aksenov/FC-heap/run.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3.4
+
+import os
+import sys
+
+def filename(warmup, duration, proc, size, benchmark):
+    return "out/data/w{}-d{}/{}_{}_{}.txt".format(warmup, duration, benchmark, proc, size)
+
+classpath = "bin"
+mainclass = "testing.Measure"
+
+warmup = 1000
+duration = 1000
+iterations = 5
+procs = [1, 2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 63]
+sizes = [800000, 10000000, 20000000]
+
+max_proc = int(sys.argv[2])
+
+benchmarks=["fc.parallel.FCParallelHeap",
+           "fc.parallel.FCHalfParallelHeap",
+           "fc.sequential.FCBinaryHeap",
+           "fc.sequential.FCPairingHeap"]
+
+if not os.path.isdir("out/w{}-d{}/".format(warmup, duration)):
+     os.makedirs("out/w{}-d{}/".format(warmup, duration))
+
+for proc in procs:
+    if proc > max_proc:
+        break
+    for size in sizes:
+        for benchmark in benchmarks:
+            out = filename(warmup, duration, proc, size, benchmark)
+            command = "java -server -cp {} {} -n {} -t {} -w {} -d {} -s {} -r {} -b {} > {}".format(classpath, mainclass, iterations, proc, warmup, duration, size, 2 * size, benchmark, out)
+            print(command)
+            os.system(command)
+
+
+
+
+
+
