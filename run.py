4beceab6711a10a0dc159c57aa325ae77293c5da
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-10 15:14:03 +0000

    Updated scripts

--- run.py
+++ run.py
@@ -3,20 +3,20 @@
 import os
 import sys
 
-def filename(warmup, duration, proc, size, benchmark):
-    return "out/log/w{}-d{}/{}_{}_{}.txt".format(warmup, duration, benchmark, proc, size)
+def filename(warmup, duration, proc, size, r, benchmark):
+    return "out/log/w{}-d{}/{}_{}_{}_{}.txt".format(warmup, duration, benchmark, proc, size, r)
 
 classpath = "bin"
 mainclass = "testing.Measure"
 
 keys = ["throughput"]
-procs = range(1, 80)#[1, 2, 4, 8, 15, 16, 23, 24, 31, 32, 39, 47, 55, 63, 71, 79]
 
 warmup = 1000
 duration = 1000
 iterations = 5
 procs = [1, 3, 7, 14, 21, 28, 35, 42, 49, 56, 63]
-sizes = [800000, 2000000, 4000000]
+sizes = [800000, 2000000, 4000000, 8000000]
+ranges = [100, 10000]
 
 max_proc = int(sys.argv[1])
 
@@ -34,11 +34,14 @@ for proc in procs:
     if proc > max_proc:
         break
     for size in sizes:
-        for benchmark in benchmarks:
-            out = filename(warmup, duration, proc, size, benchmark)
-            command = "java -server -Xmx2G -Xss256M -cp {} {} -n {} -t {} -w {} -d {} -s {} -r {} -b {} > {}".format(classpath, mainclass, iterations, proc, warmup, duration, size, 2 * size, benchmark, out)
-            print(command)
-            os.system(command)
+        allranges = list(ranges)
+        allranges.append(2 * size)
+        for r in allranges:
+            for benchmark in benchmarks:
+                out = filename(warmup, duration, proc, size, r, benchmark)
+                command = "java -server -Xmx2G -Xss256M -cp {} {} -n {} -t {} -w {} -d {} -s {} -r {} -b {} > {}".format(classpath, mainclass, iterations, proc, warmup, duration, size, r, benchmark, out)
+                print(command)
+                os.system(command)
 
 
 
