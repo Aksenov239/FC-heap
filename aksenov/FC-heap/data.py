5c0d0a6ba538188abfaae64a1d156db69699133f
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-10 15:14:03 +0000

    Updated scripts

--- aksenov/FC-heap/data.py
+++ aksenov/FC-heap/data.py
@@ -20,8 +20,8 @@ def read_from_file(filename, keys):
         values[key].append(float(value))
     return values
 
-def filename(warmup, duration, proc, size, benchmark):
-    return "out/log/w{}-d{}/{}_{}_{}.txt".format(warmup, duration, benchmark, proc, size)
+def filename(warmup, duration, proc, size, r, benchmark):
+    return "out/log/w{}-d{}/{}_{}_{}_{}.txt".format(warmup, duration, benchmark, proc, size, r)
 
 keys = ["throughput"]
 procs = range(1, 80)
@@ -29,6 +29,7 @@ procs = range(1, 80)
 warmup = 5000
 duration = 5000
 sizes = [800000, 2000000, 4000000]
+ranges = [100, 10000]
 
 benchmarks=["fc.parallel.FCParallelHeap",
            "fc.parallel.FCHalfParallelHeap",
@@ -43,13 +44,16 @@ if not os.path.isdir(directory):
 
 for key in keys:
     for size in sizes:
-        for i in range(len(benchmarks)):
-            bench = benchmarks[i]
-            out = open((directory + "comparison_{}_{}_{}.dat").format(key, size, bench.split(".")[-1]), 'w')
-            if not os.path.exists(filename(warmup, duration, 1, size, bench)):
-                continue
-            for proc in procs:
-                if not os.path.exists(filename(warmup, duration, proc, size, bench)):
+        allranges = list(ranges)
+        allranges.append(2 * size)
+        for r in allranges:
+            for i in range(len(benchmarks)):
+                bench = benchmarks[i]
+                out = open((directory + "comparison_{}_{}_{}_{}.dat").format(key, size, r, bench.split(".")[-1]), 'w')
+                if not os.path.exists(filename(warmup, duration, 1, size, r, bench)):
                     continue
-                results = read_from_file(filename(warmup, duration, proc, size, bench), keys)[key][1:]
-                out.write(str(proc) + " " + str(mean(results) / 1000 / 1000) + " " + str(stdev(results) / 1000 / 1000) + "\n")
+                for proc in procs:
+                    if not os.path.exists(filename(warmup, duration, proc, size, r, bench)):
+                        continue
+                    results = read_from_file(filename(warmup, duration, proc, size, r, bench), keys)[key][1:]
+                    out.write(str(proc) + " " + str(mean(results) / 1000 / 1000) + " " + str(stdev(results) / 1000 / 1000) + "\n")
