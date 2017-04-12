01e06f104d559a7d7cc8444fef27a61bbb08d87e
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-12 22:42:23 +0000

    revert

--- src/fc/parallel/FCHalfParallelHeap.java
+++ src/fc/parallel/FCHalfParallelHeap.java
@@ -304,11 +304,11 @@ public class FCHalfParallelHeap implements Heap {
                 leaderExists = false;
                 fc.unlock();
             } else {
-                while (request.status == Status.PUSHED && !request.leader && leaderExists) {
+                while (request.status == Status.PUSHED && !request.leader) {
                     fc.addRequest(request);
 //                    sleep();
                 }
-                if (request.leader || !leaderExists) { // Someone set me as a leader or leader does not exist
+                if (request.leader) { // Someone set me as a leader
                     continue;
                 }
                 if (request.status == Status.SIFT_DELETE) { // should know the node for sift down
