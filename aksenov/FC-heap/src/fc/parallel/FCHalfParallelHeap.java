55c7742efbe370143deec4ffe6d5fa7027f91722
aksenov <aksenov@aa7c4a97-57d5-c14e-a808-19a6e652f231>
2017-04-12 11:29:38 +0000

    remove part of the volatile

--- aksenov/FC-heap/src/fc/parallel/FCHalfParallelHeap.java
+++ aksenov/FC-heap/src/fc/parallel/FCHalfParallelHeap.java
@@ -158,8 +158,7 @@ public class FCHalfParallelHeap implements Heap {
                 }
             }
 
-            if (request.leader && !leaderInTransition &&
-                    (request.status == Status.PUSHED || request.status == Status.FINISHED)) { // I'm the leader
+            if (request.leader && request.status == Status.PUSHED) { // I'm the leader
                 fc.addRequest(request);
 
                 for (int t = 0; t < TRIES; t++) {
@@ -256,6 +255,7 @@ public class FCHalfParallelHeap implements Heap {
                                 insertRequests[insertStart++].status = Status.FINISHED;
                             } else {
                                 while (heap[heapSize].underProcessing) { // We should swap only with unprocessed vertices
+                                    heap[heapSize].underProcessing = false;
                                     heapSize--;
                                 }
                                 if (node >= heapSize - 1) { // If we again are last or already out then do nothing
